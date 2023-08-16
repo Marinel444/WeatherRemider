877a654dff6926ad5378c5fb19a2672b39228386
Marinel Marina <marinelmarina@Marinel.local>
2023-08-16 11:11:46 +0300

    add dotenv

--- DjangoWeatherRemider/DjangoWeatherRemider/settings.py
+++ DjangoWeatherRemider/DjangoWeatherRemider/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ul$o=0$nfd%&bu^@g@n3&jte3#ee_@d=%$)@+0sgd)ucz$h9e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
